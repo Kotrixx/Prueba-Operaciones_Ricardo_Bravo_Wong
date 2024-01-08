78d7f8755bccb5eb011ee88712a61f4996fbd5f2
Kotrix <a20206331@pucp.edu.pe>
2024-01-08 11:16:03 -0500

    actualizacion del archivo de base de datos y primer listado (get)

--- server-node/rest-api.js
+++ server-node/rest-api.js
@@ -5,8 +5,18 @@ const port = 3000;
 const path = require('path');
 
 //Conexion al archivo SQLite
-const db = new sqlite3.Database('../rapimoney-db.db');
-app.listen(3000,function(){
+const db = new sqlite3.Database('../rapimoneySQLite.db');
+
+app.get('/items', (req, res) => {
+  db.all('SELECT * FROM clientes', (err, rows) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json({ items: rows });
+  });
+});
+
+app.listen(3000, function () {
 	console.log(`Running in :${port}`);
 });
 
