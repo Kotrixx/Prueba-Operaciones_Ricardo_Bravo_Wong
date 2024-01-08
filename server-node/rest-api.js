14110ac425d9b4f4b226c4fbb39d569babe53e4d
Kotrix <a20206331@pucp.edu.pe>
2024-01-08 17:40:00 -0500

    agregando ajax y jquery para el consumo de API

--- server-node/rest-api.js
+++ server-node/rest-api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const path = require('path');
@@ -7,12 +8,21 @@ const path = require('path');
 //Conexion al archivo SQLite
 const db = new sqlite3.Database('../rapimoneySQLite.db');
 
+const corsOptions = {
+  origin: '*',
+  methods: 'GET', // Puedes ajustar esto según los métodos que estás utilizando (por ejemplo, 'GET, POST, PUT, DELETE')
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+
 app.get('/items', (req, res) => {
   db.all('SELECT * FROM clientes', (err, rows) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
-    res.json({ items: rows });
+    res.json(rows);
   });
 });
 
