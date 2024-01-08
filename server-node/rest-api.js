025dc883fcc752bfc5857b9412ef7805454c55d3
Kotrix <a20206331@pucp.edu.pe>
2024-01-08 01:02:14 -0500

    conexion con base de datos

--- server-node/rest-api.js
+++ server-node/rest-api.js
@@ -2,6 +2,12 @@ const express = require('express');
 const sqlite3 = require('sqlite3');
 const app = express();
 const port = 3000;
+const path = require('path');
 
+//Conexion al archivo SQLite
+const db = new sqlite3.Database('../rapimoney-db.db');
+app.listen(3000,function(){
+	console.log(`Running in :${port}`);
+});
 
 
