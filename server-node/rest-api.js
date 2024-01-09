0707420771d2de0cb6f967a90719d87b2aa5cbe1
Kotrix <a20206331@pucp.edu.pe>
2024-01-08 19:53:00 -0500

    arreglos en el webService para filtrar y ver detalles de la operacion

--- server-node/rest-api.js
+++ server-node/rest-api.js
@@ -10,22 +10,47 @@ const db = new sqlite3.Database('../rapimoneySQLite.db');
 
 const corsOptions = {
   origin: '*',
-  methods: 'GET', // Puedes ajustar esto según los métodos que estás utilizando (por ejemplo, 'GET, POST, PUT, DELETE')
+  methods: 'GET', //Metodos permitidos para CORS
   optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
 
 
-app.get('/items', (req, res) => {
-  db.all('SELECT * FROM clientes', (err, rows) => {
+//RUTA PARA LISTAR TRANSACCIONES FILTRADO POR DNI Y/O FEECHA
+app.get('/transacciones', (req, res) => {
+  const { dni, fecha } = req.query;
+
+  // Verificar si hay o no fecha
+  const date_condition = fecha ? `AND fecha_de_operacion = '${fecha}'` : '';
+
+  const query = `SELECT * FROM transacciones WHERE dni = ${dni} ${date_condition}`;
+
+  db.all(query, (err, rows) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
-    res.json(rows);
+    res.json({ rows });
   });
 });
 
+//RUTA PARA VER DETALLES DE UNA TRANSACCION MEDIANTE SU ID
+app.get('/transaccion', (req, res) => {
+  const { id } = req.query;
+  // Verificar si hay o no id
+  const id_condition = id;
+  
+  const query = `SELECT * FROM transacciones t INNER JOIN clientes c ON t.dni = c.dni WHERE id_operacion = ${id_condition}`;
+
+  db.all(query, (err, rows) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json({ rows });
+  });
+});
+
+//RUTA PARA LISTAR POR CATEGORIA (BONUS)
 
 app.listen(3000, function () {
 	console.log(`Running in :${port}`);
